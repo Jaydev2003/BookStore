470214c8e231e351663cd19816816cdbada2b97b
Jaydev <116018824+Jaydev2003@users.noreply.github.com>
2023-02-28 12:25:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './component/Title';
+import Home from './pages.js/Home';
+function App() {
+  return (
+    <div className="App">
+           <Title />
+            <Home />
+    </div>
+  );
+}
+
+export default App;
